04d719039feea4300477aa7f22e13813c3b6ad5f
yeeun22 <128441724+yeeun22@users.noreply.github.com>
2023-06-27 12:37:58 +0900

    기말과제 수정1

---
+++ assignment2/script/clickTxt.js
@@ -0,0 +1,7 @@
+const myText = document.getElementById("h1-4");
+
+myText.addEventListener("click", function () {
+  alert(
+    "디스토피아 속 사랑을 꿈꾸는 소설가. 『뉴서울파크 젤리장수 대학살』, 『칵테일, 러브, 좀비』, 『스노볼 드라이브』 등을 썼다. 스릴러, SF, 호러 등 다양한 장르에 도전하고 있다."
+  );
+});
